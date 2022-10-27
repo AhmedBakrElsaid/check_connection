4046b1c0d04940c34f4a0b924b8dde9596c4b8a2
AhmedBakrElsaid <Ahmedbakr24@std.mans.edu.eg>
2022-10-27 13:09:31 +0200

    check_connection_project

---
+++ main.js
@@ -0,0 +1,40 @@
+let title = document.querySelector('.title');
+let ul = document.querySelector('ul');
+let reload = document.querySelector('.reload');
+
+
+window.onload=function(){
+    if(window.navigator.onLine){
+        online()
+    }else{
+        offline()
+    }
+}
+
+window.addEventListener('online',function(){
+    online()
+})
+
+window.addEventListener('offline',function(){
+    offline()
+})
+
+reload.onclick = function(){
+    window.location.reload();
+}
+
+function online(){
+    title.innerHTML='online now';
+    title.style.color='green';
+    ul.classList.add('hide');
+    reload.classList.add('hide');
+
+}
+
+function offline(){
+    title.innerHTML='offline now';
+    title.style.color='#666';
+    ul.classList.remove('hide');
+    reload.classList.remove('hide');
+
+}
